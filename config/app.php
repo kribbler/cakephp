24a3123c743c1600cea8d93a590cc7cccd4fade5
Daniel Oraca <daniel.oraca@qanw.co.uk>
2016-08-25 13:52:46 +0100

    add env variables

--- config/app.php
+++ config/app.php
@@ -222,16 +222,16 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
+            'host' => 'db',
             /**
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => env('CAKEPHP_USER_NAME'),
+            'password' => env('CAKEPHP_DB_PASSWORD'),
+            'database' => env('CAKEPHP_DB_NAME'),
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
